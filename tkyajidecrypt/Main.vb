c89619dcacfa1353589396f75ea702f0da7d03cb
c0mpuwiz <vikram.kumarz@live.com>
2017-10-26 21:09:26 +0530

    Code push!

---
+++ tkyajidecrypt/Main.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Security.Cryptography
+Imports Plossum.CommandLine
+
+Module Main
+    Function Main(args As String()) As Integer
+        Dim options As New Options()
+        Dim parser As New CommandLineParser(options)
+        parser.Parse()
+        Console.WriteLine(parser.UsageInfo.GetHeaderAsString(78))
+        If options.Help Then
+            Console.WriteLine(parser.UsageInfo.GetOptionsAsString(78))
+            Return 0
+        ElseIf parser.HasErrors Then
+            Console.WriteLine(parser.UsageInfo.GetErrorsAsString(78))
+            Return -1
+        End If
+
+        Try
+            Dim aes As New RijndaelManaged
+            aes.BlockSize = 128
+            aes.KeySize = 256
+
+            aes.Mode = CipherMode.CBC
+            aes.Padding = PaddingMode.None
+            aes.Key = Text.Encoding.ASCII.GetBytes(options.CipherKey)
+            aes.IV = Text.Encoding.ASCII.GetBytes(options.CipherIV)
+
+            Dim decrypto As ICryptoTransform = aes.CreateDecryptor
+            Dim encryptedString As Byte() = Convert.FromBase64String(ReadFile(options.InputFilePath))
+            Dim decryptedString = decrypto.TransformFinalBlock(encryptedString, 0, encryptedString.Length)
+            Save(options.OutputFilePath, Text.Encoding.ASCII.GetString(decryptedString))
+            Console.WriteLine(Text.Encoding.ASCII.GetString(decryptedString))
+            Console.ReadLine()
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message.ToString)
+            Console.ReadLine()
+        End Try
+        Return 0
+    End Function
+
+    Sub Save(filepath As String, value As String)
+        Dim info As New IO.FileInfo(filepath)
+        Dim fs As IO.FileStream = info.Create()
+        fs.Write(Text.Encoding.ASCII.GetBytes(value), 0, Text.Encoding.ASCII.GetBytes(value).Length)
+        fs.Close()
+        fs.Dispose()
+    End Sub
+
+    Function ReadFile(filepath As String) As String
+        Dim fs As New IO.StreamReader(filepath, IO.FileMode.Open)
+        ReadFile = fs.ReadToEnd()
+        fs.Close()
+        fs.Dispose()
+    End Function
+
+End Module
---
+++ tkyajidecrypt/Options.vb
@@ -0,0 +1,65 @@
+﻿Imports Plossum.CommandLine
+
+<CommandLineManager(ApplicationName:="tkyaji decrypt utility",
+                    Copyright:="Copyright (c) Vikram Kumar 2017")>
+Class Options
+    <CommandLineOption(Description:="Displays this help text")>
+    Public Help As Boolean = False
+
+    <CommandLineOption(Description:="Specifies the input file path", MinOccurs:=1)>
+    Public Property InputFilePath() As String
+        Get
+            Return mInputFilePath
+        End Get
+        Set
+            If [String].IsNullOrEmpty(Value) Then
+                Throw New InvalidOptionValueException("The input file path must not be empty", False)
+            End If
+            mInputFilePath = Value
+        End Set
+    End Property
+
+    <CommandLineOption(Description:="Specifies the Cipher Key", MinOccurs:=1)>
+    Public Property CipherKey() As String
+        Get
+            Return mCipherKey
+        End Get
+        Set
+            If [String].IsNullOrEmpty(Value) Then
+                Throw New InvalidOptionValueException("The Cipher Key must not be empty", False)
+            End If
+            mCipherKey = Value
+        End Set
+    End Property
+
+    <CommandLineOption(Description:="Specifies the Cipher IV", MinOccurs:=1)>
+    Public Property CipherIV() As String
+        Get
+            Return mCipherIV
+        End Get
+        Set
+            If [String].IsNullOrEmpty(Value) Then
+                Throw New InvalidOptionValueException("The Cipher IV must not be empty", False)
+            End If
+            mCipherIV = Value
+        End Set
+    End Property
+
+    <CommandLineOption(Description:="Specifies the output file path", MinOccurs:=1)>
+    Public Property OutputFilePath() As String
+        Get
+            Return mOutputFilePath
+        End Get
+        Set
+            If [String].IsNullOrEmpty(Value) Then
+                Throw New InvalidOptionValueException("The output file path must not be empty", False)
+            End If
+            mOutputFilePath = Value
+        End Set
+    End Property
+
+    Private mInputFilePath As String
+    Private mCipherKey As String
+    Private mCipherIV As String
+    Private mOutputFilePath As String
+End Class
